ca5da72d686a67e77fffbc6e260f94c01e7217b5
Philip10023 <philipormorgan@gmail.com>
2017-06-06 12:41:23 -0400

    react

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :feeds, only: [:index, :show, :create, :new, :destroy]
   resources :reviews, only: [:index, :destroy]
 
+  namespace :api do
+    namespace :v1 do
+      resources :feeds, only: [:show]
+      resources :reviews, only: [:create, :update]
+    end
+  end
 
   devise_for :users
   resources :users, except: [:new, :create]
