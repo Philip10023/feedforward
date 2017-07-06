5a0b27391aba51fe897df4ca9024ad193d25d2c6
Philip10023 <philipormorgan@gmail.com>
2017-07-06 15:12:29 -0400

    finally found perfect video sizes for all screens, and attempting chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'feeds#index'
   resources :feeds, only: [:index, :show, :create, :new, :destroy]
   resources :reviews, only: [:index, :destroy]
+  resources :chat_rooms, only: [:new, :create, :show, :index]
 
   namespace :api do
     namespace :v1 do
