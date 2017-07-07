45f41110c2555a9667e5054016fdc16fe22abcea
Philip10023 <philipormorgan@gmail.com>
2017-07-07 17:00:39 -0400

    destroyed production db

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,9 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = false
+  config.cache_classes = true
     #^attempting to fix loading in production with false
-    
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -81,8 +81,8 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-  config.action_cable.allowed_request_origins = ['https://peaceful-ravine-73963.herokuapp.com',
-                                               'http://peaceful-ravine-73963.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://blooming-fjord-27337.herokuapp.com',
+                                               'http://blooming-fjord-27337.herokuapp.com']
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
