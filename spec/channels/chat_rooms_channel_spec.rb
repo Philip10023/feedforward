3a32cf5eb1bda8d37e8293700346c49ed8c276c7
Philip10023 <philipormorgan@gmail.com>
2017-07-18 23:04:23 -0400

    coverage rose considerably after adding eager load true to test enviornment and after i added code to support precense of ajax requests with rspec

---
+++ spec/channels/chat_rooms_channel_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+require "pry"
+
+class TestConnection
+  attr_reader :identifiers, :logger
+
+  def initialize(identifiers_hash = {})
+    @identifiers = identifiers_hash.keys
+    @logger = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(StringIO.new))
+
+    # This is an equivalent of providing `identified_by :identifier_key` in ActionCable::Connection::Base subclass
+    identifiers_hash.each do |identifier, value|
+      define_singleton_method(identifier) do
+        value
+      end
+    end
+  end
+end
+
+RSpec.describe ChatRoomsChannel do
+
+  let(:current_user) { FactoryGirl.create(:user, id: "1", username: "Bob", email: "joshmo@gmail.com") }
+  
+  subject(:channel) { described_class.new(connection, current_user) }
+
+  # Connection is `identified_by :current_profile`
+  let(:connection) { TestConnection.new(current_user: current_user) }
+  binding.pry
+  let(:action_cable) { ActionCable.server }
+
+  # ActionCable dispatches actions by the `action` attribute.
+  # In this test we assume the payload was successfully parsed (it could be a JSON payload, for example).
+  let(:data) do
+    {
+      "action" => "test_action",
+      "times_to_say_hello" => 3
+    }
+  end
+
+  it "broadcasts 'Hello, Bob!' 3 times"  do
+    expect(action_cable).to receive(:broadcast).with("1", "Hello, Bob!").exactly(3).times
+
+    channel.perform_action(data)
+  end
+end
